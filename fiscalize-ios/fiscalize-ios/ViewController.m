45c4074c212e29236724d9cb67013f884bd61226
Jhonatan Raphael <jhonatan.raphael@tecsinapse.com.br>
2015-05-08 12:31:40 -0400

    Atualizando UI nota fiscal

--- fiscalize-ios/fiscalize-ios/ViewController.m
+++ fiscalize-ios/fiscalize-ios/ViewController.m
@@ -32,7 +32,8 @@
 @property (weak, nonatomic) IBOutlet UILabel *valorLabel;
 @property (weak, nonatomic) IBOutlet UILabel *valorGlosaLabel;
 @property (weak, nonatomic) IBOutlet UILabel *valorLiquidoLabel;
-@property (weak, nonatomic) IBOutlet UITextView *descricaoLabel;
+@property (weak, nonatomic) IBOutlet UITextView *descricaoTextView;
+
 
 
 
@@ -75,7 +76,7 @@
     NSString *cota = json[@"cota"];
     NSString *unidadeFederativa = json[@"uf"];
     NSString *dataEmissão = json[@"dataEmissao"];
-    NSString *descricao = json[@"Viagem para Buenos Aires"];
+    NSString *descricao = json[@"descricao"];
     NSString *fornecedor = json[@"fornecedor"];
     NSString *cpfCnpj = json[@"cpfcnpj"];
     NSString *anoRessarcimento = json[@"ano"];
@@ -89,15 +90,27 @@
     
     // nota fiscal
     _numeroNotaLabel.text = numeroNota;
+    _cotaLabel.text = cota;
+    _fornecedorLabel.text = fornecedor;
+    _dataEmissaoLabel.text = dataEmissão;
+    _dataRessarcimentoLabel.text = [[NSString alloc] initWithFormat:@"%@/%@", mesRessarcimento, anoRessarcimento];
+    _valorLabel.text = valor;
+    _valorGlosaLabel.text = valorGlosa;
+    _valorLiquidoLabel.text = valorLiquido;
+    _descricaoTextView.text = descricao;
     
     // partido
-    [_fotoPartidoImageView sd];
+    [_fotoPartidoImageView sd_setImageWithURL:[NSURL URLWithString:urlFotoPartido] placeholderImage:nil];
     _siglaPartidoLabel.text = siglaPartido;
     _nomePartidoLabel.text = nomePartido;
     
     // parlamentar
+    [_fotoPartidoImageView sd_setImageWithURL:[NSURL URLWithString:urlFotoParlamentar] placeholderImage:nil];
     _nomeParlamentarLabel.text = nomeParlamentar;
     _emailParlamentarLabel.text = emailParlamentar;
+    _unidadeFederativaLabel.text = unidadeFederativa;
+    _cpfCnpjLabel.text = cpfCnpj;
+    
     
 }
 
