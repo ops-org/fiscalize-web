6d76d75581d97b05bbca6bc79a9025e39f9909e0
danilo <danilo@yottaapps.com.br>
2015-05-18 19:19:22 -0300

    REST autorizar e notaFiscal (aleatória)

--- src/main/java/br/net/ops/fiscalize/controller/RestController.java
+++ src/main/java/br/net/ops/fiscalize/controller/RestController.java
@@ -8,11 +8,14 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import br.net.ops.fiscalize.domain.NotaFiscal;
+import br.net.ops.fiscalize.domain.Usuario;
 import br.net.ops.fiscalize.service.RestService;
 import br.net.ops.fiscalize.webutil.BundleUtils;
 import br.net.ops.fiscalize.webutil.Utilidade;
@@ -28,10 +31,43 @@ public class RestController extends ControllerBase {
 	@Autowired
 	private RestService restService;
 	
+	@ModelAttribute
+	public Usuario newRequest(@RequestParam(required=false) Integer usuarioId, 
+							  @RequestParam(required=false) String tokenId) {
+		Usuario usuario = new Usuario();
+		usuario.setUsuarioId(usuarioId);
+		usuario.setTokenId(tokenId);
+		return usuario;
+	}
+	
 	@ResponseBody
-	@RequestMapping(value="/notaFiscal", method=RequestMethod.GET)
-    public void playlist(HttpServletResponse response, HttpServletRequest request) {
-		logger.log(Level.INFO, "GET Nota Fiscal aleatória...");
+	@RequestMapping(value="/autorizar", method=RequestMethod.POST)
+    public void autorizar(HttpServletRequest request, HttpServletResponse response, Usuario usuario) {
+		logger.log(Level.INFO, "POST - Autorizar...");
+
+		response.setContentType(Utilidade.HTTP_HEADER_JSON);
+		
+		usuario = restService.cadastrarAutorizar(usuario);
+		
+		try {
+			Gson gson = new GsonBuilder()
+				.excludeFieldsWithoutExposeAnnotation().create();
+			
+			response.getWriter().write(gson.toJson(usuario));
+		} catch(IOException e) {
+			try {
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST, BundleUtils.obterMensagem(BundleUtils.EXCEPTION_MESSAGES, "erro.rest.login"));
+			} catch (IOException e1) {
+				logger.log(Level.WARNING, "Impossível responder requisição! Admin: verificar!");
+			}
+		}
+		
+    }
+	
+	@ResponseBody
+	@RequestMapping(value="/recuperarNotaFiscal", method=RequestMethod.GET)
+    public void recuperarNotaFiscal(HttpServletResponse response, HttpServletRequest request) {
+		logger.log(Level.INFO, "GET - Nota Fiscal aleatória...");
 
 		response.setContentType(Utilidade.HTTP_HEADER_JSON);
 		
@@ -45,9 +81,9 @@ public class RestController extends ControllerBase {
 			response.getWriter().write(gson.toJson(notaFiscal));
 		} catch(IOException e) {
 			try {
-				response.sendError(HttpServletResponse.SC_BAD_REQUEST, BundleUtils.obterMensagem(BundleUtils.GLOBAL_MESSAGES, "erro.rest.notafiscal"));
+				response.sendError(HttpServletResponse.SC_BAD_REQUEST, BundleUtils.obterMensagem(BundleUtils.EXCEPTION_MESSAGES, "erro.rest.notafiscal"));
 			} catch (IOException e1) {
-				logger.log(Level.WARNING, "Impossível responder requisição de nota fiscal! Admin: verificar!");
+				logger.log(Level.WARNING, "Impossível responder requisição! Admin: verificar!");
 			}
 		}
 		
